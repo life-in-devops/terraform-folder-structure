4e909f7e231dbff1c3647d0d4b15a63a54c3b0f8
life-in-devops <lifeindevops@gmail.com>
2025-07-24 10:42:28 +0530

    Update: backend files for only passing key-value pairs

--- backend/dev-backend.tf
+++ backend/dev-backend.tf
@@ -1,9 +1,5 @@
-terraform {
-  backend "s3" {
     bucket         = "devops-projects-terraform"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "tf-lock-table"
     encrypt        = true
-  }
-}
